6754104be22ca017de201b10836e089e23c1c379
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-06-30 15:19:43 +0100

    remade and updated js files/ css & handlebars

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS new_tech_db;
+
+CREATE DATABASE new_tech_db;
---
+++ public/js/new.js
@@ -0,0 +1,50 @@
+function handlePermission() {
+  navigator.permissions.query({ name: "geolocation" }).then(function (result) {
+    if (result.state == "granted") {
+      report(result.state);
+      geoBtn.style.display = "none";
+    } else if (result.state == "prompt") {
+      report(result.state);
+      geoBtn.style.display = "none";
+      navigator.geolocation.getCurrentPosition(
+        revealPosition,
+        positionDenied,
+        geoSettings
+      );
+    } else if (result.state == "error/denied") {
+      report(result.state);
+      geoBtn.style.display = "inline";
+    }
+    result.addEventListener("change", function () {
+      report(result.state);
+    });
+  });
+}
+
+function report(state) {
+  console.log("Permission " + state);
+}
+
+handlePermission();
+
+const newFormHandler = async function (event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
+
+  await fetch(`/api/post`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  document.location.replace("/dashboard");
+};
+
+document
+  .querySelector("#new-post-form")
+  .addEventListener("submit", newFormHandler);
